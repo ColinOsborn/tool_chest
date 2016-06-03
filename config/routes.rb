2110216aecfded95994f5d3d8797afbeda3fecbf
Colin Osborn <colin.osborn@gmail.com>
2016-06-03 00:20:43 -0600

    fixing some bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  
+  delete '/login', to: 'session#destroy'
+
   resources :tools
   resources :users, only: [:new, :create, :show]
 end
